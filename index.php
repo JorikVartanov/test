3c8116969b106a30b5a5199a21b27bdfd61c1b25
JorikVartanov <jorikvartanov13@i.ua>
2014-09-08 15:08:58 +0300

    Filters: name, comment, email, url is finished.

--- index.php
+++ index.php
@@ -27,38 +27,44 @@
 					<div class="col-md-4">
 						<form method="post" class="form-horizontal" role="form">
 							<div class="form-group">
-								<label for="inputText" class="col-sm-2 control-label">Text</label>
-								<div class="col-sm-10">
+								<label for="inputText" class="col-sm-3 control-label">Text</label>
+								<div class="col-sm-9">
 									<textarea class="form-control" name="comment" id="inputComment" placeholder="Comment" rows="3"></textarea>
 								</div>
 							</div>
 							<div class="form-group">
-								<label for="inputName" class="col-sm-2 control-label">Name</label>
-								<div class="col-sm-10">
+								<label for="inputName" class="col-sm-3 control-label">Name</label>
+								<div class="col-sm-9">
 									<input type="text" name="username" class="form-control" id="inputName" placeholder="Name">
 								</div>
 							</div>
 							<div class="form-group">
-								<label for="inputEmail" class="col-sm-2 control-label">Email</label>
-								<div class="col-sm-10">
+								<label for="inputEmail" class="col-sm-3 control-label">Email</label>
+								<div class="col-sm-9">
 									<input type="email" name="email" class="form-control" id="inputEmail" placeholder="Email">
 								</div>
 							</div>
 							<div class="form-group">
-								<label for="inputPassword" class="col-sm-2 control-label">Password</label>
-								<div class="col-sm-10">
+								<label for="inputHomepage" class="col-sm-3 control-label">Homepage</label>
+								<div class="col-sm-9">
+									<input type="text" name="homepage" class="form-control" id="inputHomepage" placeholder="Homepage">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="inputPassword" class="col-sm-3 control-label">Password</label>
+								<div class="col-sm-9">
 									<input type="password" name="password" class="form-control" id="inputPassword" placeholder="Password">
 								</div>
 							</div>
 							<div class="form-group">
-								<label for="InputFile" class="col-sm-2 control-label">File input</label>
-								<div class="col-sm-10">
+								<label for="InputFile" class="col-sm-3 control-label">File input</label>
+								<div class="col-sm-9">
 									<input type="file" name="file" id="InputFile" placeholder="File">
 									<p class="help-block">Example block-level help text here.</p>
 								</div>
 							</div>
 							<div class="form-group">
-								<div class="col-sm-offset-2 col-sm-10">
+								<div class="col-sm-offset-3 col-sm-9">
 									<div class="checkbox">
 										<label>
 											<input type="checkbox"> Remember me
@@ -67,7 +73,7 @@
 								</div>
 							</div>
 							<div class="form-group">
-								<div class="col-sm-offset-2 col-sm-10">
+								<div class="col-sm-offset-3 col-sm-9">
 									<button type="submit" class="btn btn-default">Submit</button>
 								</div>
 							</div>
@@ -92,20 +98,26 @@
 				<?php
 					if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 						//Filter for $username
-						if ($_POST['username'] == '' OR !preg_match('/^[a-zA-Z0-9\'\- ]+$/', $_POST['username'])){ //The filter should allow names with quotes and dashes. For example: Shaquille O'Neal or Anastasiya Sokolova-Rosha
+						if ($_POST['username'] == '' OR !preg_match('/^[a-zA-Z0-9]+$/', $_POST['username'])){ //The filter should allow names with quotes and dashes. For example: Shaquille O'Neal or Anastasiya Sokolova-Rosha
 							$username = 'Invalid Name';
 						}
 						else{
 							$username = $_POST['username'];
 						}
-						if ($_POST['email'] != ''){
+						if ($_POST['email'] != '' OR filter_var($_POST['email'], FILTER_SANITIZE_EMAIL)){
 							$email = $_POST['email'];
 						}
 						else{
-							$email = 'Insert email';
+							$email = 'Invalid email';
+						}
+						if ($_POST['homepage'] == '' OR !filter_var($_POST['homepage'], FILTER_VALIDATE_URL)){
+							$homepage = 'Invalid homepage';
+						}
+						else{
+							$homepage = $_POST['homepage'];
 						}
 						if ($_POST['comment'] != ''){						
-							$comment = $_POST['comment'];
+							$comment = filter_var($_POST['comment'], FILTER_SANITIZE_STRING);
 						}
 						else{
 							$comment = 'Insert comment';
@@ -118,8 +130,10 @@
 						}
 						echo "Привет, $username </p>";
 						echo "Твое мыло: $email </p>";
+						echo "Твоя страница: $homepage </p>";
 						echo "Твой комментарий: $comment </p>";
 						echo "Твой пароль: $password </p>";
+						echo "REMOTE_ADDR";
 					}
 				?>
 			</div>
